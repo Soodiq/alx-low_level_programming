42f78452eb7e760227e9ed5a221eb758dd2199ac
soodiq <eniolasodiq09@gmail.com>
2022-06-09 16:45:51 -0700

    the commit

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,4 +1,4 @@
-nclude "main.h"
+#include "main.h"
 /**
  * print_line - prints a line n chars long.
  * @n: length of line.
--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -28,7 +28,7 @@ int main(void)
 		}
 		if (i != 100)
 		{
-			printf(" ")
+			printf(" ");
 		}
 	}
 	printf("\n");
